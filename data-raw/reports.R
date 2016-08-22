bf6e20940dd640b25f29d20783cea6e43641bc8d
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-08-23 02:56:41 +0800

    Fix encoding issue

--- data-raw/reports.R
+++ data-raw/reports.R
@@ -1,10 +1,15 @@
 library(magrittr)
+library(dplyr)
 
 reports <- unz("data-raw/report.csv.zip", "report .csv") %>%
   read.csv(
     stringsAsFactors = F,
     fileEncoding = "big5",
     encoding="utf8"
+    ) %>%
+  mutate_if(
+    function(col) is.character(head(col)),
+    function(col) `Encoding<-`(col, "UTF-8")
     )
 
-devtools::use_data(reports)
+devtools::use_data(reports, overwrite = T)
