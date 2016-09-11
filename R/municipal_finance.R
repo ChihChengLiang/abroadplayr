21d7cfed3140e195710e395a8197b9c78cd9fec7
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-09-12 01:56:45 +0800

    minor: doc indent

--- R/municipal_finance.R
+++ R/municipal_finance.R
@@ -7,9 +7,9 @@
 #' @source \url{http://statdb.dgbas.gov.tw/pxweb/dialog/statfile9.asp}, downloaded 2016-09-11
 #' @format Data frame with columns
 #' \describe{
-#'  \item{year}
-#'  \item{cities}
-#'  items of statistics, check foot note for detail
+#' \item{year}
+#' \item{cities}
+#' \item{financial report items}{items of statistics, check foot note for detail}
 #' }
 #' @examples
 #'    municipal_finance[municipal_finance$year == 2015,]
--- R/reports.R
+++ R/reports.R
@@ -6,33 +6,33 @@
 #' @source \url{https://drive.google.com/drive/folders/0B44pNgi6s_pGRDlsSWdUck5fZ1k}, downloaded 2016-08-22
 #' @format Data frame with columns
 #' \describe{
-#'  \item{gov}{Central or municipal government}
-#'  \item{id}{}
-#'  \item{plan_name}
-#'  \item{report_name}
-#'  \item{main_file}{Download url of the full report}
-#'  \item{other_file}{other attachment of the report}
-#'  \item{report_date}
-#'  \item{report_page}
-#'  \item{office}
-#'  \item{member_name}
-#'  \item{member_office}
-#'  \item{member_unit}
-#'  \item{member_job}
-#'  \item{member_level}
-#'  \item{member_num}{How many members in this travel}
-#'  \item{start_date}
-#'  \item{end_date}
-#'  \item{area}
-#'  \item{visit}{What are members paying a visit to}
-#'  \item{type}{Type of traveling purpose}
-#'  \item{keyword}
-#'  \item{note}
-#'  \item{topic_cat}{Category by topic}
-#'  \item{adm_cat}{Administration category}
-#'  \item{summary}
-#'  \item{word}
-#'  \item{wiki_page}
+#' \item{gov}{Central or municipal government}
+#' \item{id}
+#' \item{plan_name}
+#' \item{report_name}
+#' \item{main_file}{Download url of the full report}
+#' \item{other_file}{other attachment of the report}
+#' \item{report_date}
+#' \item{report_page}
+#' \item{office}
+#' \item{member_name}
+#' \item{member_office}
+#' \item{member_unit}
+#' \item{member_job}
+#' \item{member_level}
+#' \item{member_num}{How many members in this travel}
+#' \item{start_date}
+#' \item{end_date}
+#' \item{area}
+#' \item{visit}{What are members paying a visit to}
+#' \item{type}{Type of traveling purpose}
+#' \item{keyword}
+#' \item{note}
+#' \item{topic_cat}{Category by topic}
+#' \item{adm_cat}{Administration category}
+#' \item{summary}
+#' \item{word}
+#' \item{wiki_page}
 #' }
 #' @examples
 #'   reports_df<- reports
