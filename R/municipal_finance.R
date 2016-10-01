4d6335c12aa5ecd04633fa96392874bcdacc80d9
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-10-01 18:24:48 +0800

    fix roxygen bad markup

--- R/municipal_finance.R
+++ R/municipal_finance.R
@@ -7,14 +7,13 @@
 #' @source \url{http://statdb.dgbas.gov.tw/pxweb/dialog/statfile9.asp}, downloaded 2016-09-11
 #' @format Data frame with columns
 #' \describe{
-#' \item{year}
-#' \item{cities}
+#' \item{year}{}
+#' \item{cities}{}
 #' \item{financial report items}{items of statistics, check foot note for detail}
 #' }
 #' @examples
-#'    municipal_finance[municipal_finance$year == 2015,]
-#'
-#'    # Here are some options you can view the foot note
-#'    View(foot_note_df)
-#'    foot_note_df[46, ]
+#' municipal_finance[municipal_finance$year == 2015,]
+#' # Here are some options you can view the foot note
+#' View(foot_note_df)
+#' foot_note_df[46, ]
 "municipal_finance"
